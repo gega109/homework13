cfb3e496f02a4f28c1416e7413188fbea48b007d
gega109 <bakuravar1@gmail.com>
2024-10-17 23:57:33 +0400

    homework13
    first steps of javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let age = 21
+let adult = true;
+
+console.log("გეგა");
+console.log("ბაკურაძე");
+console.log(age);
+console.log("bakuravar1@gmail.com");
+console.error("557 71 73 50");
+console.warn(adult); 
